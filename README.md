6673cf500e5924547a5da20bce039d25e9b2b2ac
manju838 <manjunadhkandavalli@gmail.com>
2025-01-26 07:32:06 +0000

    resolving merge conflicts on gcp

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ conda activate yolov9_detection
 git clone https://github.com/propall/yolov9freshfork.git
 cd yolov9freshfork/
 
-conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+pip install torch==1.12.1 (Check cuda version first with nvcc --version, this torch version is the recent for CUDA 11.3)
 
 pip install -r requirements.txt
 
--- rescale_datasets.py
+++ rescale_datasets.py
@@ -3,61 +3,6 @@ import cv2
 import shutil
 from pathlib import Path
 
-# def resize_image_and_labels(image_path: str, label_path: str, output_image_path: str, output_label_path: str, target_size: int):
-#     """
-#     Resizes an image to a fixed size and adjusts the bounding box annotations.
-
-#     Args:
-#         image_path (str): Path to the input image file.
-#         label_path (str): Path to the input YOLO label file corresponding to the image.
-#         output_image_path (str): Path to save the resized image.
-#         output_label_path (str): Path to save the resized label annotations.
-#         target_size (int): The target size for both width and height of the image.
-
-#     Returns:
-#         None
-#     """
-#     # Read the image
-#     image = cv2.imread(image_path)
-#     h, w, _ = image.shape
-
-#     # Calculate scaling factors
-#     scale_x = target_size / w
-#     scale_y = target_size / h
-
-#     # Resize the image
-#     resized_image = cv2.resize(image, (target_size, target_size))
-
-#     # Save the resized image
-#     cv2.imwrite(output_image_path, resized_image)
-
-#     # Adjust and save the labels
-#     if os.path.exists(label_path):
-#         with open(label_path, 'r') as f:
-#             lines = f.readlines()
-
-#         resized_labels = []
-#         for line in lines:
-#             class_id, x_center, y_center, box_width, box_height = map(float, line.strip().split())
-            
-#             # Adjust bounding box coordinates
-#             x_center *= scale_x
-#             y_center *= scale_y
-#             box_width *= scale_x
-#             box_height *= scale_y
-
-#             # YOLO bounding boxes are normalized w.r.t the original image dimensions
-#             x_center /= target_size
-#             y_center /= target_size
-#             box_width /= target_size
-#             box_height /= target_size
-
-#             resized_labels.append(f"{class_id} {x_center} {y_center} {box_width} {box_height}\n")
-
-#         # Save adjusted labels
-#         with open(output_label_path, 'w') as f:
-#             f.writelines(resized_labels)
-
 
 def resize_image_and_labels(image_path: str, label_path: str, output_image_path: str, output_label_path: str, target_size: int):
     """
